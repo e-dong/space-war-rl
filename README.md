54b64a74e1808cb64d23ff799ff144578c732cf9
Eric Dong <eric2043@gmail.com>
2023-10-12 20:41:54 -0400

    Updated dev environment

--- README.md
+++ README.md
@@ -3,5 +3,15 @@
 I will be remaking the 1985 version of Space War using Pygame and train deep reinforcement learning agents to battle it out. I will try to do everything from scratch as much as possible as a learning exercise!
 
 ## First Time Setup
+
 Create a virtualenv with `python -m venv venv`. Activate it with `source venv/bin/activate`
 Install requirements with `pip install -r requirements.txt`
+
+## Starting the game
+
+python -m game
+
+## Development
+
+Install additonal dev dependencies with:
+`pip install -r requirements-dev.txt`
--- game.py
+++ game.py
@@ -1,21 +1,25 @@
-# Example file showing a circle moving on screen
+"""Example file showing a circle moving on screen"""
 import pygame
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
-running = True
-dt = 0
+RUNNING = True
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-while running:
-    # poll for events
+while RUNNING:
+    # limits FPS to 60
+    # DELTA_TIME is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    DELTA_TIME = clock.tick(60) / 1000
+
+    # event loop
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
@@ -24,21 +28,15 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= 300 * DELTA_TIME
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += 300 * DELTA_TIME
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= 300 * DELTA_TIME
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += 300 * DELTA_TIME
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
 pygame.quit()
-
