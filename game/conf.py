e190eb20130e9589ee166420a16c3c901d49c827
Eric Dong <eric2043@gmail.com>
2023-12-13 21:15:43 -0500

    Reduce weapon fire delay

--- game/conf.py
+++ game/conf.py
@@ -7,4 +7,4 @@ SCREEN_HEIGHT = 600
 # The delay in ms to check user input
 CHECK_KEYS_TIME_DELAY_MS = 100
 # The delay in ms to check firing weapons
-WEAPON_FIRE_TIME_DEPLAY_MS = 250
+WEAPON_FIRE_TIME_DEPLAY_MS = 150
