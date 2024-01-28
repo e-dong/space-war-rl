2bd51414d876410827061f7d6fdb6603465fc315
Eric Dong <eric2043@gmail.com>
2024-01-28 12:25:33 -0500

    refactor env vars

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "version=$(python setup.py --version)-dev-test" >> "$GITHUB_ENV"
+ITCH_USERNAME=e-dong
+ITCH_GAME_ID=spacewar-dev
+
+echo "BUILD_NUM=$(python setup.py --version)-dev-test" >> "$GITHUB_ENV"
+echo "ITCH_USERNAME=${ITCH_USERNAME}" >> "$GITHUB_ENV"
+echo "ITCH_GAME_ID=${ITCH_GAME_ID}" >> "$GITHUB_ENV"
