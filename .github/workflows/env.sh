6c21ea3548b0bc7a9cd0ca04cabdbc93070e4aa0
Eric Dong <eric2043@gmail.com>
2024-01-28 12:43:50 -0500

    Update to use branch and commit sha for build

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -14,9 +14,14 @@ ITCH_GAME_ID=spacewar
 BUILD_NUM=$(python setup.py --version)
 
 if [[ "${event_name}" == "pull_request" ]]; then
+  short_commit_sha=$(git rev-parse --short HEAD)
+  branch=$(git rev-parse --abbrev-ref HEAD)
+
   ITCH_GAME_ID="${ITCH_GAME_ID}-dev"
-  BUILD_NUM="${BUILD_NUM}-dev"
+  BUILD_NUM="${BUILD_NUM}-${branch}-${short_commit_sha}"
 fi
+
+echo "build_num: ${BUILD_NUM}"
 
 echo "BUILD_NUM=${BUILD_NUM}" >> "$GITHUB_ENV"
 echo "ITCH_USERNAME=${ITCH_USERNAME}" >> "$GITHUB_ENV"
