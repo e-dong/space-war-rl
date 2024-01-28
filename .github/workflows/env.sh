bf94561ccf87696ad9c6c70a1c4bb50c0a97bfa0
Eric Dong <eric2043@gmail.com>
2024-01-28 12:38:33 -0500

    Update vars based on pull_request event

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -13,6 +13,11 @@ ITCH_USERNAME=e-dong
 ITCH_GAME_ID=spacewar
 BUILD_NUM=$(python setup.py --version)
 
+if [[ "${event_name}" == "pull_request" ]]; then
+  ITCH_GAME_ID="${ITCH_GAME_ID}-dev"
+  BUILD_NUM="${BUILD_NUM}-dev"
+fi
+
 echo "BUILD_NUM=${BUILD_NUM}" >> "$GITHUB_ENV"
 echo "ITCH_USERNAME=${ITCH_USERNAME}" >> "$GITHUB_ENV"
 echo "ITCH_GAME_ID=${ITCH_GAME_ID}" >> "$GITHUB_ENV"
