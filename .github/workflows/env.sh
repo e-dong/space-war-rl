75aed2d9893a33c91e388a5f6a15fbe075329ba4
Eric Dong <eric2043@gmail.com>
2024-01-28 12:35:32 -0500

    pass workflow event_name to env.sh

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-ITCH_USERNAME=e-dong
-ITCH_GAME_ID=spacewar-dev
+event_name="${1}"
 
-echo "BUILD_NUM=$(python setup.py --version)-dev-test" >> "$GITHUB_ENV"
+if [[ -z "${event_name}" ]]; then
+  echo "[ERROR]: event name param cannot be blank"
+  exit 1
+fi
+
+echo "Setting up env vars for [${event_name}]"
+
+ITCH_USERNAME=e-dong
+ITCH_GAME_ID=spacewar
+BUILD_NUM=$(python setup.py --version)
+
+echo "BUILD_NUM=${BUILD_NUM}" >> "$GITHUB_ENV"
 echo "ITCH_USERNAME=${ITCH_USERNAME}" >> "$GITHUB_ENV"
 echo "ITCH_GAME_ID=${ITCH_GAME_ID}" >> "$GITHUB_ENV"
