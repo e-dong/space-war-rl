595da64972e7916f41d2925fb4074be624fe11c1
Eric Dong <eric2043@gmail.com>
2024-01-28 13:07:46 -0500

    Added a prefix of origin

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -16,7 +16,7 @@ BUILD_NUM=$(python setup.py --version)
 if [[ "${event_name}" == "pull_request" ]]; then
   git fetch
   git_branch="${GITHUB_HEAD_REF}"
-  git_hash="$(git rev-parse --short "${git_branch}")"
+  git_hash="$(git rev-parse --short "origin/${git_branch}")"
 
   ITCH_GAME_ID="${ITCH_GAME_ID}-dev"
   BUILD_NUM="${BUILD_NUM}-${git_branch}-${git_hash}"
