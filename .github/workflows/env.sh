45822be17cf49f7f8f0696948accdac274c1865e
Eric Dong <eric2043@gmail.com>
2024-01-28 13:02:13 -0500

    Added the correct values

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -14,11 +14,8 @@ ITCH_GAME_ID=spacewar
 BUILD_NUM=$(python setup.py --version)
 
 if [[ "${event_name}" == "pull_request" ]]; then
-  echo $GITHUB_SHA
-  echo $GITHUB_REF
-  echo $GITHUB_REF_NAME
-  git_hash=$(git rev-parse --short "$GITHUB_SHA")
-  git_branch=${GITHUB_REF#refs/heads/}
+  git_branch="${GITHUB_HEAD_REF}"
+  git_hash=$(git rev-parse --short "${git_branch}")
 
   ITCH_GAME_ID="${ITCH_GAME_ID}-dev"
   BUILD_NUM="${BUILD_NUM}-${git_branch}-${git_hash}"
@@ -30,4 +27,5 @@ echo "BUILD_NUM=${BUILD_NUM}" >> "$GITHUB_ENV"
 echo "ITCH_USERNAME=${ITCH_USERNAME}" >> "$GITHUB_ENV"
 echo "ITCH_GAME_ID=${ITCH_GAME_ID}" >> "$GITHUB_ENV"
 
-env
+# Use this to print all the exposed env vars from the workflow env
+# env
